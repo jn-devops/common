2c208c1e2e487f59755b896b706c72741fb319bc
jn-devops <devops@joy-nostalg.com>
2025-01-02 16:01:34 +0800

    add enum utils

---
+++ src/Traits/EnumUtils.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Homeful\Common\Traits;
+
+use Illuminate\Validation\Rules\In;
+use Illuminate\Validation\Rule;
+
+trait EnumUtils
+{
+    static function random(): self {
+        $enums = self::cases(); return $enums[array_rand($enums)];
+    }
+
+    static function toArray($column_key = 'value'): array {
+       return array_column(array: self::cases(), column_key: $column_key);
+    }
+
+    static function rule(): In
+    {
+        return Rule::in(self::toArray());
+    }
+}
