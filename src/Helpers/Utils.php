bdfa5d6129ebc1785f399b30ffd3d8d37244419b
jn-devops <devops@joy-nostalg.com>
2025-02-05 13:16:42 +0800

    fix titleCase to accept null

--- src/Helpers/Utils.php
+++ src/Helpers/Utils.php
@@ -80,9 +80,8 @@ if (!function_exists('dot_shift')) {
 }
 
 if (!function_exists('titleCase')) {
-    function titleCase(string $text, array $exclusions = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV']): string
+    function titleCase(string $text = '', array $exclusions = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV']): string
     {
-        // Transform the string using Str::title
         $titleCased = Str::title($text);
 
         // Restore the Roman numerals to their original case
